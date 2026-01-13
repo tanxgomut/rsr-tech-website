bfcd411f4648c88a27667bad1f5c511b7119f860
tanxgomut <tang@rush1auto.co>
2026-01-13 21:04:20 +0700

    logo 2

--- app/components/Logo.vue
+++ app/components/Logo.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex items-center gap-2 min-w-[260px]">
+  <div class="flex items-center gap-2 ">
     <NuxtImg src="/logo.jpg" alt="R.S.R Tech Group" class="h-12 rounded-full" />
     <div>
-      <p class="font-bold text-xl tracking-tight">
+      <p class="font-bold text-lg md:text-xl tracking-tight">
         R.S.R <span class="text-primary-500 dark:text-primary-400">TECH GROUP</span>
       </p>
-      <p class="text-xs">บริษัท อาร์.เอส.อาร์ เทค กรุ๊ป จำกัด</p>
+      <p class="text-[10px] md:text-xs">บริษัท อาร์.เอส.อาร์ เทค กรุ๊ป จำกัด</p>
     </div>
 
   </div>
